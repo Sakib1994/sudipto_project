ed1dcdcf8b1a495fb46b8351ba63040012dd6b65
sudiptob2 <sudiptobaral.me@gmail.com>
2020-12-02 17:50:37 +0600

    tooltip and red star done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import axios from 'axios';
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import axios from 'axios'
+import router from './router';
+import store from './store';
 
 axios.defaults.baseURL = 'http://RgadApi-env.eba-knt3svpm.us-east-1.elasticbeanstalk.com';
-axios.defaults.headers.common['x-access-token'] = localStorage.getItem('token');
-axios.defaults.headers.common['Content-Type'] = 'application/json';
+//axios.defaults.headers.common['x-access-token'] = localStorage.getItem('token');
+//axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 Vue.config.productionTip = false
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
+import router from '@/router'
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
 
-import router from '@/router'
 
 Vue.use(Vuex)
 
@@ -66,14 +66,15 @@ export default new Vuex.Store({
         geographic_preferecne: payload.geographic_preferecne,
         life_stage_preference: payload.life_stage_preference,
       };
-      // console.log(payloadJson);
+      console.log(payloadJson, localStorage.getItem('token'));
       axios.post("submit", payloadJson, 
-      // {
-      //   headers: {
-      //     'x-access-token': localStorage.getItem('token'),
-      //     'Content-Type': 'text/json'
-      //   }
-      // }
+      {
+        
+        headers: {
+          'x-access-token': localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
       ).then(response => {
         console.log(response);
         const { response_id, username, cash_distribution,
@@ -92,7 +93,7 @@ export default new Vuex.Store({
         router.push({ name: 'Result' });
       })
         .catch(error => {
-          console.log(error);
+          console.log("Errod sb ",error);
           commit("setLoadingToogle", false);
         });
       commit("setAnswers", payload);
